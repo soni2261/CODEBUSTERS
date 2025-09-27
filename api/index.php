42e2c1e028d88591bbc4e336a3560fedad5eb750
soni2261 <soniarahal20@gmail.com>
2025-09-27 05:51:29 -0400

    Configure Vercel deployment for PHP application

--- api/index.php
+++ api/index.php
@@ -1,6 +1,15 @@
 <?php
 
-require_once 'app/init.php';
+// Handle Vercel routing
+if (!isset($_GET['url'])) {
+    $_GET['url'] = $_SERVER['REQUEST_URI'] ?? '';
+    $_GET['url'] = ltrim($_GET['url'], '/');
+    if (($pos = strpos($_GET['url'], '?')) !== false) {
+        $_GET['url'] = substr($_GET['url'], 0, $pos);
+    }
+}
+
+require_once '../app/init.php';
 
 $app = new App();
 ?>
